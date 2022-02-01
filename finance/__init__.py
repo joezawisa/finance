bb14f4478b14f2a5b5ac11f5cd642dd69d8d7951
Joe Zawisa <jazawisa@umich.edu>
2022-01-31 22:46:53 -0500

    Configure logging for Green Unicorn
    Make flask and Green Unicorn's loggers play nicely together.

--- finance/__init__.py
+++ finance/__init__.py
@@ -1,6 +1,12 @@
 """Finance package initializer."""
 
 import flask
+import logging
 
 # Create an application object
 app = flask.Flask(__name__)
+
+# Configure logging for Gunicorn
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
