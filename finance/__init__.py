f128ef470130e0ede19ae8b1c2a1c1834f848d65
Joe Zawisa <jazawisa@umich.edu>
2022-02-02 02:25:01 -0500

    Create custom WSGI app
    Flask and Green Unicorn don't tend to cooperate when the application is mounted
    at a non-root path. A custom WSGI app is necessary to fix Flask's URL
    generation.

--- finance/__init__.py
+++ finance/__init__.py
@@ -6,6 +6,16 @@ import logging
 # Create an application object
 app = flask.Flask(__name__)
 
+# Create a custom WSGI app to fix Flask's URL generation
+class WSGI(object):
+    def __init__(self, app):
+        self.app = app
+    def __call__(self, environ, start_response):
+        if 'APPLICATION_ROOT' in app.config:
+            environ['SCRIPT_NAME'] = app.config['APPLICATION_ROOT']
+        return self.app(environ, start_response)
+app.wsgi_app = WSGI(app.wsgi_app)
+
 # Read settings from config module
 app.config.from_object('finance.config')
 
